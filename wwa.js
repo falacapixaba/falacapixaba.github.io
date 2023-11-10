d29bfd79e07865471b62a5806e218bd12054094d
falacapixaba <149516512+falacapixaba@users.noreply.github.com>
2023-11-10 17:39:38 -0300

    Add files via upload

---
+++ wwa.js
@@ -0,0 +1,5 @@
+const acessarLink = ()=> {
+    window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSdjrEDbmbVkhlq3WKoBJVJK7I3Ph_VIu76ab9Bxux55JJgumQ/viewform?usp=sf_link"
+}
+
+document.querySelector('#button01').addEventListener('click', acessarLink)
